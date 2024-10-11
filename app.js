9c6372f67359d1383a896d9c166979124bde3c7d
sendhyrama <sendhyrama@gmail.com>
2024-10-11 23:50:09 +0700

    chore: setup vercel deployment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 // Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
